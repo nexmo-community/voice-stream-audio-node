405064e3f1b9e84b2511b13c64200994a0ff347d
Amanda Cavallaro <amanda.cavallaro@vonage.com>
2021-11-12 15:49:08 +0000

    Changes Nexmo to Vonage

--- server.js
+++ server.js
@@ -3,25 +3,25 @@ const path = require('path')
 const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
-const Nexmo = require('nexmo')
+const Vonage = require('@vonage/server-sdk')
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
 const TO_NUMBER = process.env.TO_NUMBER
-const NEXMO_NUMBER = process.env.NEXMO_NUMBER
+const VONAGE_NUMBER = process.env.VONAGE_NUMBER
 const BASE_URL = process.env.BASE_URL
 
-const NEXMO_API_KEY = process.env.NEXMO_API_KEY
-const NEXMO_API_SECRET = process.env.NEXMO_API_SECRET
-const NEXMO_APPLICATION_ID = process.env.NEXMO_APPLICATION_ID
-const NEXMO_APPLICATION_PRIVATE_KEY_PATH = process.env.NEXMO_APPLICATION_PRIVATE_KEY_PATH
+const VONAGE_API_KEY = process.env.VONAGE_API_KEY
+const VONAGE_API_SECRET = process.env.VONAGE_API_SECRET
+const VONAGE_APPLICATION_ID = process.env.VONAGE_APPLICATION_ID
+const VONAGE_APPLICATION_PRIVATE_KEY_PATH = process.env.VONAGE_APPLICATION_PRIVATE_KEY_PATH
 
-const nexmo = new Nexmo({
-  apiKey: NEXMO_API_KEY,
-  apiSecret: NEXMO_API_SECRET,
-  applicationId: NEXMO_APPLICATION_ID,
-  privateKey: NEXMO_APPLICATION_PRIVATE_KEY_PATH
+const vonage = new Vonage({
+  apiKey: VONAGE_API_KEY,
+  apiSecret: VONAGE_API_SECRET,
+  applicationId: VONAGE_APPLICATION_ID,
+  privateKey: VONAGE_APPLICATION_PRIVATE_KEY_PATH
 })
 
 // Serve contents of public folder in the /audio path
@@ -34,14 +34,14 @@ const event_url = BASE_URL + `/webhooks/events`
 const makeOutboundCall = (req, res) => {
   console.log('Making the outbound call...')
 
-  nexmo.calls.create({
+  vonage.calls.create({
     to: [{
       type: 'phone',
       number: TO_NUMBER
     }],
     from: {
       type: 'phone',
-      number: NEXMO_NUMBER
+      number: VONAGE_NUMBER
     },
     answer_url: [answer_url],
     event_url: [event_url]
@@ -50,7 +50,7 @@ const makeOutboundCall = (req, res) => {
 
 const start_stream = (call_uuid) => {
   console.log(`streaming ${audio_url} into ${call_uuid}`)
-  nexmo.calls.stream.start(call_uuid, { stream_url: [audio_url], loop: 0 }, (err, res) => {
+  vonage.calls.stream.start(call_uuid, { stream_url: [audio_url], loop: 0 }, (err, res) => {
     if (err) {
       console.error(err)
     }
@@ -61,7 +61,7 @@ const start_stream = (call_uuid) => {
 }
 
 const stop_stream = (call_uuid) => {
-  nexmo.calls.stream.stop(call_uuid, (err, res) => {
+  vonage.calls.stream.stop(call_uuid, (err, res) => {
     if (err) {
       console.error(err)
     }
@@ -84,7 +84,7 @@ app.post('/webhooks/events', (req, res) => {
     // Disconnect the call after 20 secs
     setTimeout(() => {
       stop_stream(call_uuid)
-      nexmo.calls.update(call_uuid, { action: 'hangup' }, (req, res) => {
+      vonage.calls.update(call_uuid, { action: 'hangup' }, (req, res) => {
         console.log("Disconnecting...")
       });
     }, 20000)
